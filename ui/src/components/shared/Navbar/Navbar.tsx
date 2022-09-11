a32661b4fcfa960505cf5749bf53b9430375a476
AaryTrivedi <aarytrivedi@gmail.com>
2022-09-11 15:29:55 -0400

    Handles showing link as per authentication in navbar

--- ui/src/components/shared/Navbar/Navbar.tsx
+++ ui/src/components/shared/Navbar/Navbar.tsx
@@ -3,14 +3,37 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+
+import useIsAuthenticated from 'hooks/auth/useIsAuthenticated';
 
 function Navbar() {
+
+	const isAuthenticated = useIsAuthenticated();
+
+	debugger;
+
 	return (
 		<AppBar position="static">
 			<Toolbar>
 				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 	            	Todo
 	          	</Typography>
+	          	{
+	          		!isAuthenticated && (
+	          			<>
+	          				<Button color="inherit">Login</Button>
+	          				<Button color="inherit">Signup</Button>
+	          			</>
+          			)
+	          	}
+	          	{
+	          		isAuthenticated && (
+	          			<>
+	          				<Button color="inherit">Logout</Button>
+	          			</>
+          			)
+	          	}
           	</Toolbar>
 		</AppBar>
 	);
--- ui/src/components/shared/Navbar/__tests__/navbar.test.tsx
+++ ui/src/components/shared/Navbar/__tests__/navbar.test.tsx
@@ -2,12 +2,35 @@ import { render } from '@testing-library/react';
 
 import Navbar from '../Navbar';
 
+import LocalStorageMock from 'helpers/tests/localStorage';
+
+global.localStorage = new LocalStorageMock();
+
 describe("<Navbar />", () => {
 
 	test("It renders successfully", () => {
 		const { getByText } = render(<Navbar />);
 		const logo = getByText("Todo");
 		expect(logo).toBeInTheDocument();
+	})
+
+	test("It shows login button when user is not authenticated", () => {
+		const { getByText } = render(<Navbar />);
+		const element = getByText("Login");
+		expect(element).toBeInTheDocument();
+	})
+
+	test("It shows signup button when user is not authenticated", () => {
+		const { getByText } = render(<Navbar />);
+		const element = getByText("Signup");
+		expect(element).toBeInTheDocument();
+	})
+
+	test("It shows logout button when user is authenticated", () => {
+		localStorage.setItem("accessToken", "test");
+		const { getByText } = render(<Navbar />);
+		const element = getByText("Logout");
+		expect(element).toBeInTheDocument();
 	})
 
 })
